503a028c59c79f99bb2ad1bbc5594054d2550b61
Kamuz <jonathan.maldonado86@gmail.com>
2019-10-05 13:14:10 -0300

    primer commit

---
+++ src/main/java/server/Router.java
@@ -0,0 +1,43 @@
+package server;
+
+import controllers.UsuarioController;
+import models.UsuarioModel;
+import repositories.daos.DAOMySQL;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.utils.BooleanHelper;
+import spark.utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+    private static HandlebarsTemplateEngine engine;
+
+    private static void initEngine() {
+        Router.engine = HandlebarsTemplateEngineBuilder
+                .create()
+                .withDefaultHelpers()
+                .withHelper("isTrue", BooleanHelper.isTrue)
+                .build();
+    }
+
+    public static void init() {
+        Router.initEngine();
+        Spark.staticFileLocation("/public");
+        Router.configure();
+    }
+
+    private static void configure(){
+        UsuarioController usuarioController = new UsuarioController();
+
+        Spark.get("/usuarios", usuarioController::mostrarTodos, Router.engine);
+
+        Spark.get("/usuario/:id", usuarioController::mostrar, Router.engine);
+
+        Spark.get("/usuario", usuarioController::crear, Router.engine);
+
+        Spark.post("/usuario/:id", usuarioController::modificar);
+
+        Spark.post("/usuario", usuarioController::guardar);
+
+        Spark.delete("/usuario/:id", usuarioController::eliminar);
+    }
+}
